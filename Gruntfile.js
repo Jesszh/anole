607aa3b2104444b5e7e687695a2f2d2ab113fee8
Jess <iam@jesszh.com>
2015-02-25 17:27:50 +0800

    init

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+/*!
+ * Anole's Gruntfile
+ * http://anole.jimubox.com
+ * Copyright 2013-2015 Le Rong Duo Yuan (Beijing) technology Lid,.co.
+ * Licensed under MIT ()
+ */
+module.exports = function(grunt) {
+    'use strict';
+    // Force use of Unix newlines
+    grunt.util.linefeed = '\n';
+    RegExp.quote = function(string) {
+        return string.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&');
+    };
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*!\n' + ' * Anole v<%= pkg.version %> (<%= pkg.homepage %>)\n' + ' * Copyright 2011-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' + ' */\n',
+        stylus: {
+            options: {
+                'compress': false,
+                'include css': true,
+                'paths': ['stylus'],
+                banner: '<%= banner %>'
+            },
+            compile: {
+                files: {
+                    'dist/css/anole.css': ['stylus/anole.styl']
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['stylus/*.styl'],
+                tasks: ['stylus']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-stylus');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['stylus']);
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+What's included
+
+```
+anole/
+├── css/
+│   ├── anole.css
+│   ├── anole.min.css
+├── js/
+│   ├── anole.js
+│   └── anole.min.js
+└── fonts/
+    ├── glyphicons-halflings-regular.eot
+    ├── glyphicons-halflings-regular.svg
+    ├── glyphicons-halflings-regular.ttf
+    ├── glyphicons-halflings-regular.woff
+    └── glyphicons-halflings-regular.woff2
+```
